41ee704ad2cb5311b9681cb4404212faaf1c675b
MikeDmC <126272800+MicheleIlPro@users.noreply.github.com>
2024-10-23 14:52:44 +0200

    Add files via upload

---
+++ src/main/java/it/mikedmc/ggtodo/GgtodoApplication.java
@@ -0,0 +1,13 @@
+package it.mikedmc.ggtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgtodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/mikedmc/ggtodo/GgtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mikedmc.ggtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
