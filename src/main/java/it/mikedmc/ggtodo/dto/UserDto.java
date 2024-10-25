36050fa89a8d80a794d114960150710a06f03f58
MikeDmC <micheleilpro@libero.it>
2024-10-25 19:33:54 +0200

    Sistemati i Dto aggiungendo le Validation

--- src/main/java/it/mikedmc/ggtodo/dto/UserDto.java
+++ src/main/java/it/mikedmc/ggtodo/dto/UserDto.java
@@ -1,7 +1,18 @@
 package it.mikedmc.ggtodo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class UserDto {
+    @NotBlank(message = "Lo username non può essere vuoto")
+    @NotEmpty(message = "Lo username non può essere vuoto")
+    @NotNull(message = "Username non trovato")
     private String username;
+
+    @NotBlank(message = "La password non può essere vuota")
+    @NotEmpty(message = "La password non può essere vuota")
+    @NotNull(message = "Password non trovata")
     private String password;
 
     public String getUsername() {
