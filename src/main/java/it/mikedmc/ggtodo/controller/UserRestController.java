62a5bb67d9dade17f7977d6f13c65438b2e48be8
MikeDmC <micheleilpro@libero.it>
2024-10-25 03:01:46 +0200

    creati degli endpoint per eseguire login, register e logout

---
+++ src/main/java/it/mikedmc/ggtodo/controller/UserRestController.java
@@ -0,0 +1,52 @@
+package it.mikedmc.ggtodo.controller;
+
+import it.mikedmc.ggtodo.dto.UserDto;
+import it.mikedmc.ggtodo.dto.builder.UserDtoBuilder;
+import it.mikedmc.ggtodo.model.User;
+import it.mikedmc.ggtodo.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserRestController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody UserDto userDto) {
+        if (userService.findByUsername(userDto.getUsername()) != null) {
+            return ResponseEntity.badRequest().build();  // L'utente è già presente.
+        }
+        User user = UserDtoBuilder.fromDtoToEntity(userDto);
+        return ResponseEntity.ok(userService.registerUser(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody UserDto userDto, HttpSession session) {
+
+            User user = userService.findByUsername(userDto.getUsername());
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenziali non valide.");
+            }
+
+            session.setAttribute("user", user);
+            return ResponseEntity.ok("Login successful");
+    }
+
+    @GetMapping("/current")
+    public ResponseEntity<User> getCurrentUser(HttpSession session) {
+        User currentUser = (User) session.getAttribute("user");
+        if (currentUser == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        return ResponseEntity.ok(currentUser);
+    }
+
+}
